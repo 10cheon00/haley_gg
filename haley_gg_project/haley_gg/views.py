19a583b48edac49dc8a1de6bb4c256c454cbad75
10cheon00 <10cheon00@gmail.com>
2021-01-03 04:45:15 +0000

    modified:   haley_gg/Apps/Stats/urls.py         modified:   haley_gg/views.py

--- haley_gg_project/haley_gg/views.py
+++ haley_gg_project/haley_gg/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, reverse
-# from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect
 
-# from .Apps.stats.forms import SearchUserForm
+from .Apps.Stats.forms import SearchUserForm
 
 
 def main_page(request):
-    # form = SearchUserForm(request.GET or None)
-    # if form.is_valid():
-    #     return HttpResponseRedirect(
-    #         reverse(
-    #             "stats:users_detail",
-    #             kwargs={"name": form.cleaned_data['user_name']}
-    #         )
-    #     )
+    form = SearchUserForm(request.GET or None)
+    if form.is_valid():
+        return HttpResponseRedirect(
+            reverse(
+                "stats:users_detail",
+                kwargs={"name": form.cleaned_data['user_name']}
+            )
+        )
     context = {
-        # 'search_form': form
+        'search_form': form
     }
     return render(request, "main-page.html", context)
